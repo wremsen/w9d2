344220058e30d0b038c1aae54ddf6a017b13c228
wremsen <wcremsen@gmail.com>
2023-12-12 12:49:06 -0500

    first commit

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,4 +1,5 @@
-Add your import statements for View and Game here
+import View from "./ttt-view.js";
+import Game from "../ttt_node/game.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
