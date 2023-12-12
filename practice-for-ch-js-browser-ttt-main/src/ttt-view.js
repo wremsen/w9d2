6dd478d0a835b63e9d15691adbbf39e2e46b566b
wremsen <wcremsen@gmail.com>
2023-12-12 13:27:44 -0500

    setup board done

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -2,10 +2,23 @@ class View {
   constructor(game, el) {
     this.game = game
     this.el = el
+
+    setupBoard();
   }
   
   setupBoard() {
-    
+    const board = document.createElement('ul');
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let cell = document.createElement('li');
+        li.dataset.row = row
+        li.dataset.col = col
+
+        board.appendChild(cell);
+      }
+    }
+    this.appendChild(board);
   }
   
   handleClick(e) {
